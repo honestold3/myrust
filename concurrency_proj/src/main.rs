fff66667a2aaa1bf7489c83e935bdba2b1941bd8
honestold3 <honestold3@gmail.com>
2018-10-05 19:18:24 +0800

    concurrency

---
+++ concurrency_proj/src/main.rs
@@ -0,0 +1,20 @@
+extern crate concurrency_proj;
+
+use concurrency_proj::con::thread;
+use concurrency_proj::con::pass;
+use concurrency_proj::con::share;
+
+
+fn main() {
+    //thread::kankan();
+    //thread::kankan1();
+    //thread::kankan2();
+    //thread::kankan3();
+
+    //pass::kankan();
+    //pass::kankan1();
+    //pass::kankan2();
+
+    //share::kankan();
+    share::kankan2();
+}
